1505d3004b72d59e80b0766914269f8f1f9ab609
Koski Jenna <s2200123@BC-C02GR0BPQ6LR-MacBook-Air.local>
2022-05-10 21:37:50 +0300

    form almost ready, with css also

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -4,8 +4,42 @@ const AddRecipe = (props) => {
   return (
     <form>
       <h2>Add new recipe</h2>
-      <label htmlFor="name">Recipe name</label>
-      <input type="text" name="name" id="name" />
+      <div className="form">
+        <label htmlFor="name">Recipe name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="author">Author</label>
+        <input type="text" name="author" id="author" />
+        <label htmlFor="origin">Recipe is from</label>
+        <select name="origin" id="origin">
+          <option value="" invalid="true" hidden>
+            Select country
+          </option>
+          <option value="Finland">Finland</option>
+        </select>
+        <label htmlFor="description">Description</label>
+        <input type="text" name="description" id="description" />
+        <label htmlFor="image">Image (url)</label>
+        <input type="text" name="imageUrl" id="imageUrl" />
+        <label htmlFor="ingredients">Ingredients</label>
+        <div className="ingredient_quantity_section">
+          <div className="ingredient">
+            <label htmlFor="ingredient">Ingredient</label>
+            <input type="text" name="ingredient" id="ingredient" />
+          </div>
+          <div className="quantity">
+            <label htmlFor="quantity">Quantity</label>
+            <input type="text" name="quantity" id="quantity" />
+          </div>
+        </div>
+        <button type="submit" className="addMore">
+          Add more
+        </button>
+        <label htmlFor="instructions">Instructions</label>
+        <input type="text" name="instructions" id="instructions" />
+        <button type="submit" className="postRecipe">
+          Post recipe
+        </button>
+      </div>
     </form>
   );
 };
